e681e95913e253f49b82589564d250115ea5d23e
Dmitry Dyachkov <git@e965.ru>
2025-08-12 18:11:01 +0500

    feat: generate sourcemap, typed ref in DataProvider

--- src/DataProvider/DataProvider.tsx
+++ src/DataProvider/DataProvider.tsx
@@ -1,17 +1,16 @@
-import type { ComponentChildren, FunctionComponent } from "preact";
-import type { MutableRefObject } from "preact/compat";
+import type { ComponentChildren, FunctionComponent, RefObject } from "preact";
 import { useEffect, useRef, useState } from "preact/hooks";
 
 import resultsUrl from "../data/result.json?url";
 
-import { DataContext } from "./DataContext";
+import { DataContext, type DataContextValue } from "./DataContext";
 
 type DataProviderProps = {
   children?: ComponentChildren;
 };
 
 export const DataProvider: FunctionComponent<DataProviderProps> = ({ children }) => {
-  const results = useRef<any>(null);
+  const results = useRef<DataContextValue["results"]>(null);
 
   const [loading, setLoading] = useState(false);
 
@@ -37,7 +36,7 @@ export const DataProvider: FunctionComponent<DataProviderProps> = ({ children })
 
 const loadData = async (
   setLoading: (loading: boolean) => void,
-  resultsRef: MutableRefObject<any>,
+  resultsRef: RefObject<DataContextValue["results"]>,
 ) => {
   setLoading(true);
 
